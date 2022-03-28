42be6b16f7f339a2d3d3cf45dcaf01b6c5cf3f86
xcsh <zhangweikun_xcsh@163.com>
2022-03-28 19:31:47 +0800

    git@github.com:xcsh-hit-zwk/FoodCommentAdmin.git

---
+++ src/main/java/com/example/foodcommentadmin/FoodCommentAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodcommentadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCommentAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCommentAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodcommentadmin/FoodCommentAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodcommentadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCommentAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
