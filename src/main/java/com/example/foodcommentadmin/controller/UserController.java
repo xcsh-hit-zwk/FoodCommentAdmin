e3bc7afb0d83b36de50e4b51b27da0932e61c9ff
xcsh <zhangweikun_xcsh@163.com>
2022-04-13 16:08:22 +0800

    删除了UserController中一个无用import

--- src/main/java/com/example/foodcommentadmin/controller/UserController.java
+++ src/main/java/com/example/foodcommentadmin/controller/UserController.java
@@ -5,7 +5,6 @@ import com.example.foodcommentadmin.enums.ResultCode;
 import com.example.foodcommentadmin.pojo.Account;
 import com.example.foodcommentadmin.pojo.User;
 import com.example.foodcommentadmin.service.UserService;
-import org.omg.PortableInterceptor.SUCCESSFUL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
